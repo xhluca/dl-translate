a6747ffb5c61612381de07fb6c7633b358eaf9aa
Xing Han Lu <github@xinghanlu.com>
2021-03-11 03:02:45 +0000

    Fix: dl_translate.utils
    Previous changes broken the module, this commit fixes the problems

--- dl_translate/utils.py
+++ dl_translate/utils.py
@@ -1,17 +1,14 @@
 from ._pairs import _PAIRS_MBART50
 
-# Weights dictionaries
-__MBART50 = {
-    "langs": tuple(pair[0] for pair in _PAIRS_MBART50),
-    "codes": tuple(pair[1] for pair in _PAIRS_MBART50),
-    "pairs": _PAIRS_MBART50,
-}
-
 
 def _dict_from_weights(weights: str) -> dict:
     """Returns a dictionary of lang, codes, pairs if the provided weights is supported."""
     if weights.lower() in ["mbart50", "mbart-large-50-many-to-many-mmt"]:
-        return dict(_PAIRS_MBART50)
+        return {
+            "langs": tuple(pair[0] for pair in _PAIRS_MBART50),
+            "codes": tuple(pair[1] for pair in _PAIRS_MBART50),
+            "pairs": dict(_PAIRS_MBART50),
+        }
     else:
         error_message = f"Incorrect argument '{weights}' for parameter weights. Currently, only 'mbart50' is available."
         raise ValueError(error_message)
