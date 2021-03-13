380717e3f3c251091b8eaf197e1f76c8e826a692
xhlulu <xhlperso@gmail.com>
2021-03-12 20:45:25 -0500

    Add more utils to utils

--- dl_translate/utils.py
+++ dl_translate/utils.py
@@ -1,14 +1,18 @@
 from ._pairs import _PAIRS_MBART50
 
 
+def _weights2pairs():
+    return {
+        'mbart-large-50-many-to-many-mmt': _PAIRS_MBART50,
+        'mbart50': _PAIRS_MBART50,
+        'facebook/mbart-large-50-many-to-many-mmt': _PAIRS_MBART50
+    }
+
 def _dict_from_weights(weights: str) -> dict:
     """Returns a dictionary of lang, codes, pairs if the provided weights is supported."""
-    if weights.lower() in ["mbart50", "mbart-large-50-many-to-many-mmt"]:
-        return {
-            "langs": tuple(pair[0] for pair in _PAIRS_MBART50),
-            "codes": tuple(pair[1] for pair in _PAIRS_MBART50),
-            "pairs": dict(_PAIRS_MBART50),
-        }
+    if weights.lower() in _weights2pairs():
+        return _weights2pairs[weights.lower()]
+    
     else:
         error_message = f"Incorrect argument '{weights}' for parameter weights. Currently, only 'mbart50' is available."
         raise ValueError(error_message)
